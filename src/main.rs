91b103def9416415edad698221ac49a0dd305d1d
pissapisa500-debug <pissapisa500@gmail.com>
2026-05-24 20:01:45 +0300

    Create main.rs

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    println!("Post-Quantum Web3 Security Tools");
+    println!("Hybrid cryptography prototype starting...");
+    // TODO: Implement hybrid signature scheme
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_placeholder() {
+        assert_eq!(2 + 2, 4);
+    }
+}
